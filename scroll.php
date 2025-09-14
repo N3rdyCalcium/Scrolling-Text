08384a343a5a8e69a1d22ada9340a95f05e1cc33
Calvin <calvin@Calvins-MacBook-Air.local>
2025-09-14 21:49:23 +0700

    Add Fullscreen API but didn't work unless DevTools were open

--- scroll.php
+++ scroll.php
@@ -4,7 +4,15 @@
         <title>Your full Scrolling Board</title>
         <link rel=stylesheet href="stylefull.css">
     </head>
-    <body class="animated-box">
-        <marquee direction="left" behavior="scroll" scrollamount="100"><?php echo $_REQUEST["message"] ?></marquee>
+    <body>
+        <div class="animated-box">
+            <marquee direction="left" behavior="scroll" scrollamount="100"><?php echo $_REQUEST["message"] ?></marquee>
+        </div>
+        <button onclick="enterFullscreen()" id="fsBtn" style="align-items: right;">Fullscreen</button>
+        <script>
+            function enterFullscreen() {
+                document.documentElement.requestFullscreen();
+            }
+        </script>
     </body>
 </html>
