d759c646d17d08c7db2ee66ccd661023fa9ce501
Calvin <calvin@Calvins-MacBook-Air.local>
2025-09-16 21:01:25 +0700

    add share link feature

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ function scrollingText() {
     const text = document.getElementById('text').value;
     const messageElement = document.getElementById('message');
     const hiddenInput = document.getElementById('hiddenMessage')
+    const shareLink = document.getElementById('shareLink');
     const visible = document.getElementById('visible');
 
     messageElement.textContent = text;
@@ -12,6 +13,7 @@ function scrollingText() {
     messageElement.parentNode.replaceChild(newMarquee, messageElement);
 
     hiddenInput.value = text;
+    shareLink.value = "https://pro70crazy.wuaze.com/HTMLtest/scrolling_text/scroll.php?message=" + encodeURIComponent(text).replace(/%20/g, "+");
     visible.style.display = "block";
 }
 
@@ -27,7 +29,26 @@ function refresh() {
   location.reload();
 }
 
+function copyLink() {
+  // Get the text field
+  var copyText = document.getElementById("shareLink");
+
+  // Select the text field
+  copyText.select();
+  copyText.setSelectionRange(0, 99999); // For mobile devices
+
+   // Copy the text inside the text field
+  navigator.clipboard.writeText(copyText.value);
+
+  // Alert the copied text
+  string.innerText = "Copied to Clipboard!"
+  setTimeout(() => {
+    string.innerText = "Copy Link"
+  }, 1000);
+}
+
 function enterFullscreen() {
+  console.log("Button clicked");
   if (!document.fullscreenElement) {
     document.documentElement.requestFullscreen();
     fsBtn.innerText = "Exit Fullscreen";
@@ -36,3 +57,4 @@ function enterFullscreen() {
     fsBtn.innerText = "Fullscreen";
   }
 }
+document.getElementById("fsBtn").addEventListener("click", enterFullscreen);
